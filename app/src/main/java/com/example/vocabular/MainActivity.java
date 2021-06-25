1756a3cfa241dd229912959d42cf93c7f2a2000c
Sergey <seborgey@yandex.ru>
2021-06-25 17:31:53 +0500

    Обнуление проекта

--- app/src/main/java/com/example/vocabular/MainActivity.java
+++ app/src/main/java/com/example/vocabular/MainActivity.java
@@ -1,29 +1,13 @@
 package com.example.vocabular;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView textObjectPartA;
-    TextView textObjectPartB;
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button buttonPlay = (Button)findViewById(R.id.saveButton);
-        buttonPlay.setOnClickListener(this);
-        textObjectPartA = (TextView)findViewById(R.id.editText);
-        textObjectPartB = (TextView)findViewById(R.id.textView);
-    }
-
-    @Override
-    public void onClick(View view) {
-        CharSequence word;
-        word = textObjectPartA.getText();
-        textObjectPartB.setText(word);
     }
 }
