b8e556df941550603897db81cc6f3f4c0869b538
Sergey <seborgey@yandex.ru>
2021-06-27 14:09:52 +0500

    Главное активити

--- app/src/main/java/com/example/vocabular/MainActivity.java
+++ app/src/main/java/com/example/vocabular/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.vocabular;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -14,9 +22,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.opencsv.CSVWriter;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -28,35 +42,95 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.net.ssl.HttpsURLConnection;
 
+
 public class MainActivity extends AppCompatActivity {
+    final String FILENAME = "file";
+
+    final String DIR_SD = "MyFiles";
+    final String FILENAME_SD = "fileSD";
     private String TAG = this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView contentView = (TextView) findViewById(R.id.content);
-        WebView webView = (WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
+//        TextView contentView = (TextView) findViewById(R.id.content);
+//        WebView webView = (WebView) findViewById(R.id.webView);
+//        webView.getSettings().setJavaScriptEnabled(true);
         Button btnFetch = (Button)findViewById(R.id.downloadBtn);
-
+        Button save = (Button)findViewById(R.id.saveButton);
+        EditText Text1 = (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText Text2 = (EditText) findViewById(R.id.editTextTextPersonName2);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                isStoragePermissionGranted();
+                String word1 = Text1.getText().toString();
+                String word2 = Text2.getText().toString();
+//                File sdPath = Environment.getExternalStorageDirectory();
+//                sdPath = new File(sdPath.getAbsolutePath() + "/" + "Vocabular");
+//                if (!sdPath.mkdirs()) Log.d(TAG, "НЕ СОЗДАНО");
+                File file = new File(getExternalFilesDir(null), "Мой словарь.csv");
+//                File file = new File(sdPath, "data.csv");
+//                File file = new File(csv);
+                if (!file.exists()) {
+                    try {
+                        // создаем объект FileWriter с файлом в качестве параметра
+                        FileWriter outputfile = new FileWriter(file);
+                        // создаем объект файлового объекта CSVWriter в качестве параметра
+                        CSVWriter writer = new CSVWriter(outputfile);
+                        // добавляем заголовок в csv
+                        String[] header = {"Eng", "Ru", "Learn"};
+                        writer.writeNext(header);
+                        // добавить данные в csv
+                        String[] data1 = {word1, word2, "0"};
+                        writer.writeNext(data1);
+                        // закрываем соединение с писателем
+                        writer.close();
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Сохранено", Toast.LENGTH_SHORT);
+                        toast.show();
+                    } catch (IOException e) {
+                        // TODO автоматически сгенерированный блок catch
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    try{
+                        CSVWriter writer = new CSVWriter(new FileWriter(file, true));
+                        String [] record = {word1, word2, "0"};
+                        writer.writeNext(record);
+                        writer.close();
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                        "Сохранено", Toast.LENGTH_SHORT);
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         btnFetch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                contentView.setText("Загрузка...");
+                String word = Text1.getText().toString();
+                String word_url = "https://www.multitran.com/m.exe?l1=1&l2=2&s="+word;
+//                contentView.setText("Загрузка...");
                 new Thread(new Runnable() {
                     public void run() {
                         try{
-                            String content = getContent("https://www.multitran.com/m.exe?l1=1&l2=2&s=cat");
-                            webView.post(new Runnable() {
-                                public void run() {
-                                    webView.loadDataWithBaseURL("https://www.multitran.com/m.exe?l1=1&l2=2&s=hi",content, "text/html", "UTF-8", "https://www.multitran.com/m.exe?l1=1&l2=2&s=cat");
-                                    Toast.makeText(getApplicationContext(), "Данные загружены", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                            contentView.post(new Runnable() {
+                            String content = getContent(word_url);
+//                            webView.post(new Runnable() {
+//                                public void run() {
+//                                    webView.loadDataWithBaseURL("https://www.multitran.com/m.exe?l1=1&l2=2&s=cat",content, "text/html", "UTF-8", "https://www.multitran.com/m.exe?l1=1&l2=2&s=cat");
+//                                    Toast.makeText(getApplicationContext(), "Данные загружены", Toast.LENGTH_SHORT).show();
+//                                }
+//                            });
+                            runOnUiThread(new Runnable() {
                                 public void run() {
 //                                    contentView.setText(content);
                                     Document doc = Jsoup.parse(content);
@@ -67,28 +141,49 @@ public class MainActivity extends AppCompatActivity {
 //                                    contentView.setText(doc.title());
 //                                    String name = metaElement.attr("name");a[href*=/search/]
 //                                    String linkInnerH = link.attr("s");
-                                    String linkInnerH = "";
+//                                    String[] linkInnerH = {};
+                                    List<String> listA = new ArrayList<String>();
+
                                     int i=0;
                                     for (Element links : link.select("a[href$=1]")) {
 //                                        linkInnerH = linkInnerH + links.text()+"\n";
-                                        linkInnerH = linkInnerH + links.html()+"\n";
+                                        listA.add(links.html());
+//                                        linkInnerH = linkInnerH + links.html()+"\n";
                                         // get the value from href attribute
 //                                        System.out.println("\nLink : " + links.attr("href"));
 //                                        System.out.println("Text : " + links.text());
-                                        if (i>5) break;
+                                        if (i>10) break;
                                         i++;
                                     }
+                                    String[] linkInnerH = listA.toArray(new String[0]);
+//                                    String[] linkInnerH = (String[]) listA.toArray();
+//                                    String[] linkInnerH = {"fsdf", "sfsdfs"};
 //                                    String linkInnerH = link.html();
-                                    contentView.setText(linkInnerH);
+                                    ListView lvMain = (ListView) findViewById(R.id.List);
+
+                                    // создаем адаптер
+                                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                                            android.R.layout.simple_list_item_1, linkInnerH);
+
+                                    // присваиваем адаптер списку
+                                    lvMain.setAdapter(adapter);
+                                    lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                        public void onItemClick(AdapterView<?> parent, View view,
+                                                                int position, long id) {
+                                            String selectedFromList = (String) (lvMain.getItemAtPosition(position));
+                                            Text2.setText(selectedFromList);
+                                        }
+                                    });
+//                                    contentView.setText(linkInnerH);
 
 //                                    Log.d(TAG, content);
                                 }
                             });
                         }
                         catch (IOException ex){
-                            contentView.post(new Runnable() {
+                            runOnUiThread(new Runnable() {
                                 public void run() {
-                                    contentView.setText("Ошибка: " + ex.getMessage());
+//                                    contentView.setText("Ошибка: " + ex.getMessage());
                                     Toast.makeText(getApplicationContext(), "Ошибка", Toast.LENGTH_SHORT).show();
                                 }
                             });
@@ -127,6 +222,24 @@ public class MainActivity extends AppCompatActivity {
             if (connection != null) {
                 connection.disconnect();
             }
+        }
+    }
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
         }
     }
 }
